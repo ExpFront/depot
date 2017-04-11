257d26e711fb55fb77a541c82b231590b57532ef
Andrew Abarykov <expfront@gmail.com>
2017-04-11 23:10:22 +0300

    feat(depot): update db

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -26,16 +26,18 @@ class LineItemsController < ApplicationController
   def create
     @cart = current_cart
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product.id)
 
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart,
           notice: 'Line item was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @line_item }
+        format.json { render json: @line_item,
+          status: :created, location: @line_item }
       else
-        format.html { render action: 'new' }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.html { render action: "new" }
+        format.json { render json: @line_item.errors,
+          ostatus: :unprocessable_entity }
       end
     end
   end
